def34becddfb2adee85a3327ec840a8f1d90421a
antonio-luciano <antonio-luciano@users.noreply.github.com>
2025-05-13 01:55:19 +0000

    deploy: antonio-luciano/tonhocloud-private@6557c6e5e6a70f1a60cb47b3b541faa6ad332794

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747098900',resources: [
+    cacheName: 'chirpy-1747101318',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
